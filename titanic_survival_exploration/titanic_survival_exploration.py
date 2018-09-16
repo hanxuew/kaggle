ef23f162b897776084a9357c7e4eaac467e685ed
Han <hanxwer@foxmail.com>
2018-09-16 20:25:27 +0800

    Add files via upload

--- titanic_survival_exploration/titanic_survival_exploration.py
+++ titanic_survival_exploration/titanic_survival_exploration.py
@@ -5,3 +5,53 @@ Created on Sun Sep 16 17:01:52 2018
 @author: Han
 """
 
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Pretty display for notebooks
+#%matplotlib inline
+
+# Set a random seed
+import random
+random.seed(42)
+
+# Load the dataset
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+
+# Print the first few entries of the RMS Titanic data
+display(full_data.head())
+
+# Store the 'Survived' feature in a new variable and remove it from the dataset
+outcomes = full_data['Survived']
+features_raw = full_data.drop('Survived', axis = 1)
+
+# Show the new dataset with 'Survived' removed
+display(features_raw.head())
+#将对特征进行独热编码
+features = pd.get_dummies(features_raw)
+#现在用 0 填充任何空白处
+features = features.fillna(0.0)
+display(features.head())
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, outcomes, test_size=0.2, random_state=42)
+# Import the classifier from sklearn
+from sklearn.tree import DecisionTreeClassifier
+
+# Define the classifier, and fit it to the data
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+# Making predictions
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+# Calculate the accuracy
+from sklearn.metrics import accuracy_score
+train_accuracy = accuracy_score(y_train, y_train_pred)
+test_accuracy = accuracy_score(y_test, y_test_pred)
+print('The training accuracy is', train_accuracy)
+print('The test accuracy is', test_accuracy)
